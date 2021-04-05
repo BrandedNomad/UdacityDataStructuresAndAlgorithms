38a91e142be4a0cbd9b2ebcbc9a0014e13813ebc
BrandedNomad <info@brandednomad.com>
2021-04-05 12:54:56 +0700

    initial commit

---
+++ Notes.py
@@ -0,0 +1,23 @@
+
+# The average processor can process about 1 billion instructions
+# per second, which is approx 1 nanosecond/instruction
+
+
+#Computational problems
+# Problems that have and ideal input and desired output
+# There is a relationship between the input and output
+# Requires a process for transforming inputs into the desired output
+
+#Solving Computational problems
+# 0. Dont Panic
+# 1. Consider the Inputs
+#    a. What are the inputs?
+#    b. How should the inputs be represented?
+# 2. Consider the Outputs
+#    a. What are the desired outputs
+# 3. Consider the Transformations
+#    a. What is the relationship between inputs and outputs?
+#    b. Work through some examples by hand (psuedocode)?
+#    c. Which cases are not being handled?
+# 4. Create simple mechanical solution
+
